aa63121665f33d8da541ad9bc1e893bfcc48ae3d
Cforcharming <43200223+Cforcharming@users.noreply.github.com>
2022-09-09 01:09:19 +0800

    Complete attack graph generation algorithms.

--- parsers/attack_graph_parser.py
+++ parsers/attack_graph_parser.py
@@ -9,7 +9,7 @@ from parsers import vulnerability_parser
 def generate_attack_graph(networks: dict[str, dict[str, set]], exploitable_vulnerabilities: dict[str, dict[str, dict]],
                           scores: dict[str, int], executor: ProcessPoolExecutor, single_exploit: bool) \
         -> (dict[str, nx.DiGraph], dict[str, dict[((str, str), (str, str)), str]], int):
-    # TODO
+    
     """Main pipeline for the attack graph generation algorithm."""
     
     print('Attack graphs of subnets generation started.')
@@ -51,6 +51,7 @@ def update_by_networks(networks: dict[str, dict[str, set]], attack_graph: dict[s
     futures: list[Future] = list()
     
     for network in affected_networks:
+        
         future = executor.submit(generate_sub_graph, networks, network, exploitable_vulnerabilities, scores,
                                  single_exploit)
         future.add_done_callback(update(attack_graph, graph_labels, network))
@@ -75,31 +76,77 @@ def generate_sub_graph(networks: dict[str, dict[str, set]], network: str,
     """Breadth first search approach for generation of nodes and edges
     without generating attack paths."""
     
-    # TODO
-    
     sub_graph = nx.DiGraph()
     sub_labels: dict[((str, str), (str, str)), str] = {}
     
     gateways: set[str] = networks[network]['gateways']
     neighbours: set[str] = networks[network]['nodes']
+    
+    exploited_vulnerabilities: set[str] = set()
+    
     for gateway in gateways:
         
-        gateway_post_privileges: dict[int, str] = exploitable_vulnerabilities[gateway]['post']
+        if gateway == 'outside':
+            gateway_post_privileges = {4: ''}
+        else:
+            gateway_post_privileges: dict[int, str] = exploitable_vulnerabilities[gateway]['post']
+        
+        exploited_nodes: set[str] = {gateway}
         
         for current_privilege in gateway_post_privileges:
-            for neighbour in neighbours:
-                for neighbour_pre_condition in range(0, current_privilege + 1):
-                    if len(exploitable_vulnerabilities[neighbour]['pre']) > 0:
-                        for vulnerability in exploitable_vulnerabilities[neighbour]['pre'][neighbour_pre_condition]:
-                            neighbour_post_condition = exploitable_vulnerabilities[neighbour]['postcond'][vulnerability]
-                            add_edge(sub_graph, sub_labels, gateway, current_privilege, neighbour, neighbour_post_condition, vulnerability)
+            depth_first_search(gateway, neighbours, current_privilege, exploited_nodes, exploited_vulnerabilities,
+                               exploitable_vulnerabilities, scores, sub_graph, sub_labels, single_exploit)
     
-    print('Generated sub attack graph for network', network)
+    print('Generated sub attack graph for network', network, flush=True)
     return sub_graph, sub_labels
+
+
+def depth_first_search(exploited_node: str, neighbours: set[str], current_privilege: int, exploited_nodes: set[str],
+                       exploited_vulnerabilities: set[str], exploitable_vulnerabilities: dict[str, dict[str, dict]],
+                       scores: dict[str, int], sub_graph: nx.DiGraph, sub_labels: dict[((str, str), (str, str)), str],
+                       single_exploit: bool):
+    
+    for neighbour in neighbours:
+        
+        if neighbour == 'outside':
+            continue
+        
+        for neighbour_pre_condition in range(0, current_privilege + 1):
+            if len(exploitable_vulnerabilities[neighbour]['pre']) > 0:
+                for vulnerability in exploitable_vulnerabilities[neighbour]['pre'][neighbour_pre_condition]:
+                    neighbour_post_condition = exploitable_vulnerabilities[neighbour]['postcond'][vulnerability]
+                    if vulnerability not in scores:
+                        pass
+                    
+                    score = scores[vulnerability]
+                    start_node = (exploited_node, vulnerability_parser.get_privilege_level(current_privilege))
+                    end_node = (neighbour, vulnerability_parser.get_privilege_level(neighbour_post_condition))
+                    
+                    if single_exploit:
+                        if neighbour not in exploited_nodes:
+                            exploited_nodes.add(neighbour)
+                            add_edge(sub_graph, sub_labels, start_node, end_node, vulnerability, score)
+                            depth_first_search(neighbour, neighbours, neighbour_post_condition, exploited_nodes,
+                                               exploited_vulnerabilities, exploitable_vulnerabilities, scores,
+                                               sub_graph, sub_labels, single_exploit)
+                    
+                    else:
+                        if (start_node, end_node) not in sub_labels \
+                                 or vulnerability not in sub_labels[(start_node, end_node)]:
+                            exploited_vulnerabilities.add(vulnerability)
+                            add_edge(sub_graph, sub_labels, start_node, end_node, vulnerability, score)
+                            depth_first_search(neighbour, neighbours, neighbour_post_condition, exploited_nodes,
+                                               exploited_vulnerabilities, exploitable_vulnerabilities, scores,
+                                               sub_graph, sub_labels, single_exploit)
+
 
 def add_edge(sub_graph: nx.DiGraph, sub_labels: dict[((str, str), (str, str)), str], start_node: (str, str),
              end_node: (str, str), vulnerability: str, score: int):
     """
     Adding an edge to the attack graph.
     """
-    # TODO
+    sub_graph.add_edge(start_node, end_node, weight=score)
+    if (start_node, end_node) in sub_labels:
+        sub_labels[(start_node, end_node)] += '\n' + vulnerability
+    else:
+        sub_labels[(start_node, end_node)] = vulnerability
