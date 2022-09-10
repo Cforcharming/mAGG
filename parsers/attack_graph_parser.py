615cfd309d976801add25454c79c7013723fef1f
Cforcharming <43200223+Cforcharming@users.noreply.github.com>
2022-09-10 16:18:36 +0800

    depth first remove of redundant nodes

--- parsers/attack_graph_parser.py
+++ parsers/attack_graph_parser.py
@@ -54,6 +54,23 @@ def get_graph_compose(attack_graph: dict[str, nx.DiGraph],
     return composed_graph, composed_labels, dcg
 
 
+# noinspection PyCallingNonCallable
+def remove_redundant(composed_graph: nx.DiGraph, composed_labels: dict[((str, str), (str, str)), str]):
+    depth_delete_stack = []
+    
+    for node in composed_graph:
+        
+        if composed_graph.in_degree(node) == 0:
+            print(node)
+            (name, privilege) = node
+            if name != 'outside':
+                depth_delete_stack.append(node)
+    print(depth_delete_stack)
+    
+    while len(depth_delete_stack) > 0:
+        depth_first_remove(composed_graph, composed_labels, depth_delete_stack)
+
+
 def update_by_networks(networks: dict[str, dict[str, set]], attack_graph: dict[str, nx.DiGraph],
                        graph_labels: dict[str, dict[((str, str), (str, str)), str]],
                        exploitable_vulnerabilities: dict[str, dict[str, dict]], scores: dict[str, int],
@@ -169,3 +186,26 @@ def add_edge(sub_graph: nx.DiGraph, sub_labels: dict[((str, str), (str, str)), s
 def get_weight_from_score(score: int) -> float:
     weight = math.pow(10, -score)
     return weight
+
+
+# noinspection PyCallingNonCallable
+def depth_first_remove(composed_graph: nx.DiGraph, composed_labels: dict[((str, str), (str, str)), str],
+                       depth_delete_stack: list[(str, str)]):
+
+    node_to_remove = depth_delete_stack.pop()
+    edges_to_remove = []
+    end_nodes = set()
+    
+    out_edges = composed_graph.out_edges(node_to_remove)
+    for out_edge in out_edges:
+        (start_node, end_node) = out_edge
+        edges_to_remove.append(out_edge)
+        del composed_labels[out_edge]
+        end_nodes.add(end_node)
+    
+    for end_node in end_nodes:
+        
+        if composed_graph.in_degree(end_node) == 0:
+            depth_delete_stack.append(end_node)
+
+    composed_graph.remove_node(node_to_remove)
